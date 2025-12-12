085f11fffef488df1ea6907b85b8581dd7be352c
Renatolbn <renatolbn@gmail.com>
2025-12-12 15:05:46 -0300

    Primeiros exercícios Javascript!!

---
+++ Projetos/Calculator/assets/scripts/app.js
@@ -0,0 +1,13 @@
+const defautResult = 0;
+let currentResult = defautResult;
+
+function add(n1, n2) {
+    const result = n1 + n2;
+    return result;
+}
+
+addBtn.addEventListener();
+
+outputResult(currentResult, calculationDescription);
+
+// Javascript lê o código de cima para baixo.
